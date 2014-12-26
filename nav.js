2923fc56ef057cc89d3e4ed9460e9dc25988d240
Tulin Akdogan <tulinakdogan@hotmail.com>
2014-12-26 02:51:09 +0200

    initial commit

---
+++ nav.js
@@ -0,0 +1,15 @@
+/*
+  Slidemenu
+*/
+(function() {
+	console.log("bok ye");
+  var $body = document.body;
+	var $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
+
+	if ( typeof $menu_trigger !== 'undefined' ) {
+		$menu_trigger.addEventListener('click', function() {
+			$body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
+		});
+	}
+
+}).call(this);
