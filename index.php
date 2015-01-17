59744b54780be8b33079b49522069bd1f61ac72c
Tulin Akdogan <tulinakdogan@hotmail.com>
2015-01-17 14:12:18 -0500

    added procfile and php test

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php 
 
+session_start();
+
+if (isset($_SESSION["count"]) {
+  $_SESSION["count"] += 1;
+}
+else {
+  $_SESSION["count"] = 0;
+}
+    
 include_once("tulin.html"); 
 
+echo $_SESSION["count"];    
+    
 ?>
 
