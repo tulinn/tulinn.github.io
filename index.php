63b2d913149311a9e70a19725f3ec1919896059f
Tulin Akdogan <tulinakdogan@hotmail.com>
2015-11-07 18:33:57 -0500

    removed the test counter

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php 
-
-session_start();
-
-if (isset($_SESSION["count"])) {
-  $_SESSION["count"]++;
-}
-else {
-  $_SESSION["count"] = 0;
-}
     
 include_once("tulin.html"); 
 
-echo $_SESSION["count"];    
     
 ?>
 
