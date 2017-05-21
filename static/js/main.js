33d8d40a02abbcf606ae74caf0ee1151240dbd63
Tulin Akdogan <tulinakdogan@hotmail.com>
2017-05-20 20:04:44 -0400

    Fixed navbar highlighting functionality

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,13 @@
-$(".nav a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
+$(document).ready(function() {
+	$(".nav").find(".active").removeClass("active");
+
+	var selectedNavbarIndex;	
+    var path = window.location.pathname;
+    if (path == "/") {
+    	selectedNavbarIndex = 0;
+    } else if (path == "/about/") {
+    	selectedNavbarIndex = 1;
+    }
+
+    $(".nav").children().eq(selectedNavbarIndex).addClass("active");
 });
